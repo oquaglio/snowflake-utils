da4a79478f5f5bc8058aeae4140bf0ac6729b333
Otto Quaglio <otto.q@gmx.com>
2024-11-06 17:36:39 +0800

    fix formatting

--- test_conn.py
+++ test_conn.py
@@ -29,25 +29,29 @@ from cryptography.hazmat.primitives.serialization import (
 )
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
-#import getpass
+
+# import getpass
 import logging
 import snowflake.connector
 
+
 def test_snowflake_connectivity_using_connector(conn_params):
 
-    password = None if conn_params['private_key_file_pwd'] == "" else conn_params['private_key_file_pwd']
+    password = (
+        None if conn_params["private_key_file_pwd"] == "" else conn_params["private_key_file_pwd"]
+    )
 
-    print(f"Testing with key: {conn_params['private_key_file']}")	
+    print(f"Testing with key: {conn_params['private_key_file']}")
 
     if password is not None:
         print(f"First char of KEY_PASSWORD is: {password[0]}")
 
-    private_key_path = conn_params.get('private_key_file')
+    private_key_path = conn_params.get("private_key_file")
     if not private_key_path:
         raise ValueError("Private key file path is not specified.")
-	
-    with open(conn_params['private_key_file'], "rb") as key_file:
-	private_key_data = key_file.read()
+
+    with open(conn_params["private_key_file"], "rb") as key_file:
+        private_key_data = key_file.read()
 
     print("PCKS#8 Key Header:")
     private_key_string = private_key_data.decode("utf-8")
@@ -59,7 +63,7 @@ def test_snowflake_connectivity_using_connector(conn_params):
 
         # Check if the private key is encrypted
         if b"ENCRYPTED" in private_key_data:
-            password = conn_params.get('private_key_file_pwd')
+            password = conn_params.get("private_key_file_pwd")
             if not password:
                 raise ValueError("Password required but not provided for encrypted private key.")
             password_bytes = password.encode()
@@ -72,12 +76,10 @@ def test_snowflake_connectivity_using_connector(conn_params):
             backend=default_backend(),
         )
 
-	print("Private key loaded successfully.")
-    
-    except Exception as e:
-    	print(f"Error loading private key: {e}")
+        print("Private key loaded successfully.")
 
-	
+    except Exception as e:
+        print(f"Error loading private key: {e}")
 
     # Ensure 'pem' is still bytes
     assert isinstance(pem, bytes), "Private key must be bytes"
@@ -92,7 +94,7 @@ def test_snowflake_connectivity_using_connector(conn_params):
 
     # Create a connection
     print("Creating conn...")
-    conn = snowflake.connector.connect(**conn_params)    
+    conn = snowflake.connector.connect(**conn_params)
     print("Created conn [OK]")
 
     print("Getting cursor")
@@ -113,23 +115,23 @@ def test_snowflake_connectivity_using_connector(conn_params):
 
 if __name__ == "__main__":
 
-	import argparse
-	import json
-	
-	# Create the parser
-	parser = argparse.ArgumentParser(description="Read JSON input from a file.")
-	# Add an argument to accept a file path
-	parser.add_argument('--json-file', type=str, required=True, help='File path for JSON data.')
-	
-	# Parse the arguments
-	args = parser.parse_args()
-	
-	# Read JSON data from a file and convert to Python dictionary
-	try:
-		with open(args.json_file, 'r') as file:
-			conn_params = json.load(file)
-			print("Connection Parameters:", conn_params)
-	except (FileNotFoundError, json.JSONDecodeError) as e:
-		print("Error reading from file:", e)
-	
-	test_snowflake_connectivity_using_connector(conn_params)
+    import argparse
+    import json
+
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Read JSON input from a file.")
+    # Add an argument to accept a file path
+    parser.add_argument("--json-file", type=str, required=True, help="File path for JSON data.")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Read JSON data from a file and convert to Python dictionary
+    try:
+        with open(args.json_file, "r") as file:
+            conn_params = json.load(file)
+            print("Connection Parameters:", conn_params)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print("Error reading from file:", e)
+
+    test_snowflake_connectivity_using_connector(conn_params)
